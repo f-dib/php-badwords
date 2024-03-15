bffacc853ded19b9b98186ad48251c2a0dbe4141
federico-dibitonto <f.dibitonto94@gmail.com>
2024-03-15 17:52:11 +0100

    start exercise and added element to the index and link to result.php page

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
+<?php 
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BadWords</title>
+    <title>Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>ciao a tutti</h1>
+    <div class="container-xxl p-5">
+        <div class="d-flex flex-column justify-content-center align-items-center gap-5">
+            <h1 class="text-danger">Il Censuratore</h1>
+        
+            <form action="results.php">
+                <div class="form-floating">
+                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                    <label for="floatingTextarea">Inserisci Testo da Censurare</label>
+                </div>
+                <div>
+                    <label for="">Inserisci la parola da censurare</label>
+                    <input name="bad-word" type="text" placeholder="Inserisci parola">
+                </div>
+                <input type="submit">
+            </form>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ results.php
@@ -0,0 +1,18 @@
+<?php 
+
+$bad_word = $_GET['bad-word'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BadWords</title>
+</head>
+<body>
+    <h1>ciao a tutti</h1>
+    <?php echo $bad_word ?>
+</body>
+</html>
