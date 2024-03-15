50817603b5e69781026ba2eff556143ce04fcccd
federico-dibitonto <f.dibitonto94@gmail.com>
2024-03-15 16:24:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BadWords</title>
+</head>
+<body>
+    <h1>ciao a tutti</h1>
+</body>
+</html>
