1e99eae11ce1e90b9b848ab609bbab0c583f6d35
federico-dibitonto <f.dibitonto94@gmail.com>
2024-03-18 09:19:01 +0100

    exercise completed, added text length, style to fix

--- results.php
+++ results.php
@@ -2,6 +2,7 @@
 
 $bad_word = $_GET['bad-word'];
 $censored_text = $_GET['censored-text'];
+$text_length = strlen($censored_text);
 $new_text = str_replace($bad_word, "****", $censored_text);
 
 ?>
@@ -12,6 +13,7 @@ $new_text = str_replace($bad_word, "****", $censored_text);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BadWords</title>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
@@ -19,8 +21,12 @@ $new_text = str_replace($bad_word, "****", $censored_text);
     <div class="container-xxl p-5">
 
         <div class="d-flex flex-column text-center gap-5">
-            <h1 class="text-danger">Ecco il tuo testo censurato</h1>
-            <?php echo $new_text ?>
+            <h1 class="text-danger">Ecco il tuo documento censurato</h1>
+            <h5>Numero di caratteri presenti nel testo: <?php echo $text_length ?></h5>
+            <div class="my_sheet d-flex flex-column">
+
+                <?php echo $new_text ?>
+            </div>
         </div>
 
     </div>
